ec9ca6bb9284e0dcb246745a25bf33144e467cf7
Ronak Patel <ronak1.patel@ryerson.ca>
2019-03-16 16:16:26 -0400

    Create foundational files for the core of the Portfolio site.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Header from "./Header";
+import Resume from "./Resume";
+import Skills from "./Skills";
+import Contact from "./Contact";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <Header />
+      <div className="ui container">
+        <Resume />
+      </div>
+      <div className="ui container">
+        <Skills />
+      </div>
+      <div className="ui container">
+        <Contact />
+      </div>
+    </div>
+  );
 };
 
 export default App;

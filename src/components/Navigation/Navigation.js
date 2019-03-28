7b9a0e9d2e1f17685a299c36198bd0d1e392ba21
Ronak Patel <ronak1.patel@ryerson.ca>
2019-03-27 21:33:07 -0400

    fixed navigation bar, made it fixed to the top of the page and increased the size to massive. Added icons to the footer section for my contact info.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,24 +6,36 @@ import { Menu, Segment } from "semantic-ui-react";
 import "./Navigation.css";
 
 export default class Header extends Component {
+  state = { activeItem: "" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
+
     return (
-      <Segment inverted color="black" size="large">
-        <Menu inverted pointing secondary fixed="top" size="massive" fluid>
-          <Menu.Item name="home">
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item name="aboutme">
-            <Link to="/">About Me</Link>
-          </Menu.Item>
-          <Menu.Item name="skills">
-            <Link to="/">Skills</Link>
-          </Menu.Item>
-          <Menu.Item name="contact">
-            <Link to="/">Contact</Link>
-          </Menu.Item>
-        </Menu>
-      </Segment>
+      <Menu inverted pointing secondary fixed="top" size="massive">
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="skills"
+          active={activeItem === "skills"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="experience"
+          active={activeItem === "experience"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="contact"
+          active={activeItem === "contact"}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
     );
   }
 }
--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -5,17 +5,33 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div class="footer" style={{}}>
-      <a href="https://www.linkedin.com/in/ronakpatel6/" target="_blank">
+    <div class="footer">
+      <a
+        href="https://www.linkedin.com/in/ronakpatel6/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Icon name="linkedin" id="linkedin" size="huge" />
       </a>
-      <a href="https://medium.com/@ronakpatel0609" target="_blank">
+      <a
+        href="https://medium.com/@ronakpatel0609"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Icon name="medium m" id="medium" size="huge" />
       </a>
-      <a href="https://www.github.com/Ronak06" target="_blank">
+      <a
+        href="https://www.github.com/Ronak06"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Icon name="github" id="github" size="huge" />
       </a>
-      <a href="mailto:ronak1.patel@ryerson.ca" target="_blank">
+      <a
+        href="mailto:ronak1.patel@ryerson.ca"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Icon name="mail outline" id="mail" size="huge" />
       </a>
       <p>

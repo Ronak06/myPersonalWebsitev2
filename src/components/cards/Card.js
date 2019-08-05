5b312500caea4824d169d8055be2ebb5342cc1d8
Ronak Patel <ronak1.patel@ryerson.ca>
2019-08-05 19:57:19 -0400

    Removed all images as they are no longer required. Removed useState in Card component as it is unnecessary.

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import StackIcons from "./StackIcons";
 
 const Card = props => {
-  const [style, setStyle] = useState({
-    img: {
-      maxWidth: "400px"
-    }
-  });
+  const img = {
+    maxWidth: "400px"
+  };
 
   return (
     <div className="column">
       <div className="ui fluid card">
         <div className="image">
-          <img style={style.img} src={props.data.image} alt={style.img} />
+          <img style={img} src={props.data.image} alt={img} />
         </div>
         <div className="content">
           <StackIcons data={props.data.stack} />
@@ -23,13 +21,23 @@ const Card = props => {
           <div className="description">{props.data.description}</div>
         </div>
         <div className="extra content">
-          <a className="iconP" href={props.data.github} target="_blank">
+          <a
+            className="iconP"
+            href={props.data.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="code">
               <i className="large github icon" />
               <span />
             </span>
           </a>
-          <a className="iconP" href={props.data.website} target="_blank">
+          <a
+            className="iconP"
+            href={props.data.website}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="demo">
               <span />
             </span>
